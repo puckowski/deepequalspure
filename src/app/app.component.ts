ca626ffab02dfcd25c4d56769ef3742ae286ae87
Daniel Puckowski <puckowski.d@gmail.com>
2024-03-24 07:52:10 -0400

    Update to version 2.0.0
    * Update to ngx-deep-equals-pure version 2.0.0.
    * Show Angular version in navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VERSION } from '@angular/core';
 
 @Component({
   selector: 'eq-pure-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DeepEqualsPure';
+  public angularVersion = VERSION.full;
 }
